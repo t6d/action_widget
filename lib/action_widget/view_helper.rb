f9c5d7b456f3dce4845cbae6fcf3ea4e83f82c73
Konstantin Tennhard <me@t6d.de>
2012-12-04 11:18:14 +0100

    Changes the parameter list of ActionWidget::Base#initialize
    The constructor now requires the view context to be passed as first argument

--- lib/action_widget/view_helper.rb
+++ lib/action_widget/view_helper.rb
@@ -1,9 +1,9 @@
 module ActionWidget
   module ViewHelper
-    
+
     def method_missing(name, *args, &block)
       super unless name =~ /_widget$/
-    
+
       klass = begin
        @_action_widget_class_cache       = {}
        @_action_widget_class_cache[name] = "#{name.to_s.camelcase}".constantize
@@ -12,9 +12,10 @@ module ActionWidget
       rescue LoadError => e
         super
       end
-    
-      klass.new(*args).render_in_context(self, &block)
+
+      klass.new(self, *args).render(&block)
     end
 
   end
 end
+
