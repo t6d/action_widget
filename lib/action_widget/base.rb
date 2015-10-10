6a213780c379cce3806d15f48d2d8fa7de084b6a
Konstantin Tennhard <me@t6d.de>
2015-10-09 23:55:14 -0400

    Additional render arguments and options

--- lib/action_widget/base.rb
+++ lib/action_widget/base.rb
@@ -3,6 +3,17 @@ module ActionWidget
     alias view __getobj__
     include SmartProperties
 
+    attr_reader :options
+
+    def initialize(view, attributes = {})
+      attributes = attributes.dup
+      properties = self.class.properties
+
+      @options = attributes.delete_if { |name, value| properties.key?(name) }
+
+      super(view, attributes)
+    end
+
     def render
       raise NotImplementedError, "#{self.class} must implement #render"
     end
